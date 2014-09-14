993be2e598cb23f58d03710366fa59d182df681e
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-09-14 15:53:07 -0400

    Finalizing address decoder class

--- Project1/AddressDecoder.h
+++ Project1/AddressDecoder.h
@@ -2,30 +2,38 @@
 #define __ADDRESS_DECODER_H__
 
 #include <CustomTypes.h>
-#include <cmath>
 
 # define DEFAULT_ADDR_SIZE    32U
 namespace CacheSimulator
 {
-  class AddressHelper
+  class AddressDecoder
   {
     public:
       // parameterized constructor for variable address size
-      AddressHelper(ui16 sets, ui16 block_size, ui8 address_size)
+      AddressDecoder(ui16 sets, ui16 block_size, ui8 address_size)
       {
         init(sets, block_size, address_size);
       }
       // parameterized constructor assuming 32 bit addresses
-      AddressHelper(ui16 sets, ui16 block_size)
+      AddressDecoder(ui16 sets, ui16 block_size)
       {
         init(sets, block_size, DEFAULT_ADDR_SIZE);
       }
       // function to get tag
-      ui32 tag(ui32 addr);
+      ui32 tag(ui32 addr)
+      {
+        return addr&_tagMask;
+      }
       // function to get index
-      ui32 index(ui32 addr);
+      ui32 index(ui32 addr)
+      {
+        return addr&_indexMask;
+      }
       // function to get block offset
-      ui32 bo(ui32 addr);
+      ui32 bo(ui32 addr)
+      {
+        return addr&_boMask;
+      }
     private:
       // no of tag bits = address_size - (index_bits + blockoffset_bits)
       ui16 _tagBits;
@@ -57,6 +65,28 @@ namespace CacheSimulator
         _indexBits = find_log2(sets);
         _tagBits = address_size - (_boBits + _indexBits);
 
+        // generating bitmask for block offset
+        _boMask = 0U;
+        for(ui8 i=0U; i<_boBits; i++)
+        {
+          _boBits = _boBits<<1U + 1U;
+        }
+        
+        // generating bitmask for index
+        _indexMask = 0U;
+        for(ui8 i=0; i<_indexBits; i++)
+        {
+          _indexMask = _indexMask<<1U + 1U;
+        }
+        _indexMask = _indexMask<<_boBits;
+
+        // generating bitmask for tag
+        _tagMask = 0U;
+        for(ui8 i=0; i<_tagBits; i++)
+        {
+          _tagMask = _tagMask<<1U + 1U;
+        }
+        _tagMask = _tagMask<<(_boBits+_indexBits);
       }
   };
 };
