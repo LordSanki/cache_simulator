154a8c593477fe20053e8cd2ffb6fbf9566c262d
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-10-06 06:05:46 -0400

    fixing verification file

--- Project1/verifyB.sh
+++ Project1/verifyB.sh
@@ -3,8 +3,8 @@
 ./sim_cache 32 1024 8 256 0 0 perl_trace.txt > valid8.txt
 ./sim_cache 128 1024 2 1024 4096 4 gcc_trace.txt > valid9.txt
 ./sim_cache 64 8192 2 1024 16384 4 perl_trace.txt > valid10.txt
-diff -iyw --suppress-common-lines valid5.txt ValidationRun5.txt
-diff -iyw  --suppress-common-lines valid4.txt ValidationRun4.txt
-diff -iyw  --suppress-common-lines valid3.txt ValidationRun3.txt
-diff -iyw  --suppress-common-lines valid2.txt ValidationRun2.txt
-diff -iyw  --suppress-common-lines valid1.txt ValidationRun1.txt
+diff -iyw --suppress-common-lines valid6.txt Validation6_PartB.txt
+diff -iyw  --suppress-common-lines valid7.txt Validation7_PartB.txt
+diff -iyw  --suppress-common-lines valid8.txt Validation8_PartB.txt
+diff -iyw  --suppress-common-lines valid9.txt Validation9_PartB.txt
+diff -iyw  --suppress-common-lines valid10.txt Validation10_PartB.txt
